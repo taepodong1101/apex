ab9f3841a58e84b47fbcd132c728ec91778657a1
taepodong1101 <sattam_11@hotmail.com>
2013-08-29 22:57:32 +1000

    Update DOWNLOAD_MY_FILE.sql

--- DOWNLOAD_MY_FILE.sql
+++ DOWNLOAD_MY_FILE.sql
@@ -2,16 +2,16 @@
 -- Credits: decasse from OTN forum: https://kr.forums.oracle.com/forums/thread.jspa?threadID=634578
 
 
-    Procedure download_my_file(p_file In Number) As
-    v_length    Number;
-    v_file_name Varchar2(2000);
-    Lob_loc     Bfile;
+    Procedure download_my_file( p_file In Number) As
+                                v_length    Number;
+                                v_file_name Varchar2(2000);
+                                Lob_loc     Bfile;
   
   Begin
-    Select file_name
-      Into v_file_name
-      From UpDownFiles F
-     Where File_id = p_file;
+    SELECT file_name
+    INTO v_file_name
+    FROM UpDownFiles F
+    WHERE File_id = p_file;
   
     Lob_loc  := bfilename('UPDOWNFILES_DIR', v_file_name);
     v_length := dbms_lob.getlength(Lob_loc);
@@ -22,4 +22,4 @@
     owa_util.http_header_close;
     wpg_docload.download_file(Lob_loc);
    
-  End download_my_file;
+  END download_my_file;
